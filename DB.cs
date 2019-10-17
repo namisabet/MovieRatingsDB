44af23dfcfb3378d6aa5fee4332148583d910bb1
nami sabet <nami.sabet@hotmail.com>
2019-10-17 15:57:12 -0400

    Add project files.

---
+++ DB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRatingsDB
+{
+    public static class DB
+    {
+        private static MovieRatingsEntities myDB = new MovieRatingsEntities();
+        public static IEnumerable<Movie> GetMovies()
+        {
+            return myDB.Movies.ToList();
+        }
+
+        public static Movie GetMovie(int id)
+        {
+            return myDB.Movies.Find(id);
+        }
+        public static IEnumerable<Rating> GetRatings()
+        {
+            return myDB.Ratings.ToList();
+        }
+
+        public static Rating GetRating(int id)
+        {
+            return myDB.Ratings.Find(id);
+        }
+
+    }
+}
